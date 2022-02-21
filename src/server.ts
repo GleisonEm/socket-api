1e557d9595de527902cd0c98e0ac712ea3683aff
Gleison Emanuel <gleison.spfc1208@hotmail.com>
2022-02-21 16:05:17 -0300

    add

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,16 @@
 import * as express from "express";
 import * as socketio from "socket.io";
 import * as path from "path";
+import * as fs from "fs"
+import { Translate } from "@google-cloud/translate/build/src/v2";
+// const {Translate} = require('@google-cloud/translate').('');
+var googleTranslate = require('google-translate')(apiKey, options);
+interface Result {
+  contexto: string;
+  classe: string;
+}
+
+const translate = new Translate();
 
 const app = express();
 app.set("port", process.env.PORT || 3000);
@@ -11,7 +21,46 @@ let http = require("http").Server(app);
 let io = require("socket.io")(http);
 
 app.get("/", (req: any, res: any) => {
-  res.sendFile(path.resolve("./client/index.html"));
+console.log(fs);
+
+    var data = (fs.readFileSync(path.resolve(__dirname,'file/temp.txt'), 'utf-8')).split(';');
+
+    let result: Array<Result> = [];
+    let resultTranslate: Array<String> = [];
+    data.forEach(async function(data, key, object) {
+      var index = data.split('\\');
+
+      if (index.length > 1) {
+        console.log(index.length);
+
+        // googleTranslate.translate('My name is Brandon', 'es', function(err: any, translation: any) {
+        //   console.log(translation.translatedText);
+        //   // =>  Mi nombre es Brandon
+        // });
+        // let [translations] = await translate.translate('dog', 'pt');
+        // translations = Array.isArray(translations) ? translations : [translations];
+        // console.log('Translations:');
+        // translations.forEach((translation: String, i: Number) => {
+        //   console.log(`${translation}`);
+        // });
+
+        result.push({
+            contexto: index[index.length - 2],
+            classe: index[index.length - 1]
+          });
+      }
+    });
+
+    console.log(result)
+    res.send(result);
+  // res.sendFile(path.resolve("./client/index.html"));
+});
+
+app.get("/translate", (req: any, res: any) => {
+  res.send('hello world')
+  fs.readFile("temp.txt", function(err, buf) {
+    console.log(err);
+  });
 });
 
 // whenever a user connects on port 3000 via
