67b0af42afbbaf8d3eb1e8f3d56245763481ba45
Gleison Emanuel <gleison.spfc1208@hotmail.com>
2022-03-03 18:28:46 -0300

    adjusts

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import * as express from "express";
 import * as socketio from "socket.io";
 import * as path from "path";
+import axios from "axios";
 
 const app = express();
 app.set("port", process.env.PORT || 3000);
@@ -10,8 +11,31 @@ let http = require("http").Server(app);
 // http server.
 let io = require("socket.io")(http);
 
-app.get("/", (req: any, res: any) => {
-  res.sendFile(path.resolve("./client/index.html"));
+app.get("/user1", async (req: any, res: any) => {
+  const response = await axios.get('http://192.168.0.106:3333/message', {
+      data: {
+        conversationId: "6219124fc392c345cde298e10"
+      }
+    });
+
+    console.log('deu certo', response.data)
+  res.sendFile(path.resolve("./client/user1.html"));
+});
+
+// app.get("/user1", async (req: any, res: any) => {
+//   const response = await axios.get('http://192.168.0.106:3333/message', {
+//       data: {
+//         conversationId: "6219124fc392c345cde298e10"
+//       }
+//     });
+
+//     console.log('deu certo', response.data)
+//     console.log(__dirname +"/client/user1.html");
+//   res.render(path.resolve("./client/user1.html"), {messages:response.data});
+// });
+
+app.get("/user2", (req: any, res: any) => {
+  res.sendFile(path.resolve("./client/user2.html"));
 });
 
 // whenever a user connects on port 3000 via
@@ -19,8 +43,15 @@ app.get("/", (req: any, res: any) => {
 io.on("connection", function(socket: any) {
 //   console.log("a user connected");
   // whenever we receive a 'message' we log it out
-  socket.on("message", function(message: any) {
-    console.log(message);
+  socket.on("message", async function(content: any) {
+    console.log(content);
+    const response = await axios.post('http://192.168.0.106:3333/message', {
+      message: content.message,
+      conversationId: content.conversationId,
+      userSendId: content.userSendId
+    });
+
+    console.log('deu certo', response);
   });
 });
 
